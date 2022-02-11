aa7e53a2d090a53a6b3c5de683fc45207fb21f7f
Ernesto Tapia <74267145+ErnestoTapia22@users.noreply.github.com>
2022-02-11 15:34:19 -0500

    2

---
+++ WSGClienteCM/Controllers/WebHookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WSGClienteCM.Controllers
+{
+    public class WebHookController : Controller
+    {
+        [HttpPost("UpdateStatus")]
+        public static void WriteErrorLog(string issue)
+        {
+            try
+            {
+           
+
+            }
+            catch (Exception e)
+            {
+               
+            }
+
+        }
+    }
+}
---
+++ WSGClienteCM/Models/WebHookPayloadModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WSGClienteCM.Models
+{
+    public class WebHookPayloadModel
+    {
+        public long Timestamp { get; set; }
+        public string WebhookEvent { get; set; }
+        public string issue_event_type_name { get; set; }
+
+    }
+}
